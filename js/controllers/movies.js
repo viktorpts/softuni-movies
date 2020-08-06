147bb1a9ed2ac09873493f0e22b381c02f18d26e
Viktor Kostadinov <viktor.pts@abv.bg>
2020-08-06 16:45:16 +0300

    Added pagination template and style

--- js/controllers/movies.js
+++ js/controllers/movies.js
@@ -6,7 +6,8 @@ export default async function catalog() {
     this.partials = {
         header: await this.load('./templates/common/header.hbs'),
         footer: await this.load('./templates/common/footer.hbs'),
-        movie: await this.load('./templates/movie/movie.hbs')
+        paging: await this.load('./templates/common/paging.hbs'),
+        movie: await this.load('./templates/movie/movie.hbs'),
     };
 
     const page = this.params.page || 1;
